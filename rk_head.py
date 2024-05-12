795f4aa0ade77a0cb9bbe1016baf35eb514b1e62
kaylor <kaylor.chen@qq.com>
2024-05-12 17:53:39 +0800

    add obb

--- rk_head.py
+++ rk_head.py
@@ -1,3 +1,4 @@
+import math
 def detect_forward(self, x):
     shape = x[0].shape
     y = []
@@ -19,3 +20,22 @@ def segment_forward(self, x):
         relocated.extend([mc[i]])
     relocated.extend([p])
     return relocated
+
+def obb_forward(self, x):
+        """Concatenates and returns predicted bounding boxes and class probabilities."""
+        bs = x[0].shape[0]  # batch size
+        # angle = torch.cat([self.cv4[i](x[i]).view(bs, self.ne, -1) for i in range(self.nl)], 2)  # OBB theta logits
+        angle = [self.cv4[i](x[i]) for i in range(self.nl)]
+        # NOTE: set `angle` as an attribute so that `decode_bboxes` could use it.
+        # angle = (angle.sigmoid() - 0.25) * math.pi  # [-pi/4, 3pi/4]
+        # angle = angle.sigmoid() * math.pi / 2  # [0, pi/2]
+        x = self.detect(self, x)
+        bo = len(x) // 3
+        relocated = []
+        for i in range(len(angle)):
+            relocated.extend(x[i * bo: (i + 1) * bo])
+            tmp = (angle[i].sigmoid() - 0.25) * math.pi  # [-pi/4, 3pi/4]
+            relocated.extend([tmp])
+        return relocated
+
+
