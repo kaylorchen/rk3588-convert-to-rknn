205718242246490532836539f4ae5c44f46c0992
kaylorchen <kaylor.chen@qq.com>
2024-05-17 18:04:50 +0800

    Update

---
+++ export_pose.py
@@ -0,0 +1,80 @@
+import rk_head
+from io import BytesIO
+import onnx
+import torch
+from ultralytics import YOLO
+from ultralytics.nn.modules import head
+import argparse
+
+try:
+    import onnxsim
+except ImportError:
+    onnxsim = None
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-w", "--weights", type=str, required=True, help="PyTorch yolov8 weights"
+    )
+    parser.add_argument("--opset", type=int, default=11, help="ONNX opset version")
+    parser.add_argument("--sim", action="store_true", help="simplify onnx model")
+    parser.add_argument(
+        "--input-shape",
+        nargs="+",
+        type=int,
+        default=[1, 3, 640, 640],
+        help="Model input shape only for api builder",
+    )
+    parser.add_argument("--device", type=str, default="cpu", help="Export ONNX device")
+    args = parser.parse_args()
+    assert len(args.input_shape) == 4
+    return args
+
+
+def main(args):
+    setattr(head.Detect, "forward", rk_head.detect_forward)
+    setattr(head.Pose, "forward", rk_head.pose_forward)
+    setattr(head.Pose, "kpt_decode", rk_head.pose_kpt_decode)
+    YOLOv8 = YOLO(args.weights)
+    model = YOLOv8.model.fuse().eval()
+    model.to(args.device)
+    fake_input = torch.randn(args.input_shape).to(args.device)
+    for _ in range(2):
+        model(fake_input)
+    save_path = args.weights.replace(".pt", ".onnx")
+    output_names = [
+        "output0_box",
+        "output0_class",
+        "output0_kpt",
+        "output1_box",
+        "output1_class",
+        "output1_kpt",
+        "output2_box",
+        "output2_class",
+        "output2_kpt",
+    ]
+    with BytesIO() as f:
+        torch.onnx.export(
+            model,
+            fake_input,
+            f,
+            opset_version=args.opset,
+            input_names=["images"],
+            output_names=output_names,
+        )
+        f.seek(0)
+        onnx_model = onnx.load(f)
+    onnx.checker.check_model(onnx_model)
+    if args.sim:
+        try:
+            onnx_model, check = onnxsim.simplify(onnx_model)
+            assert check, "assert check failed"
+        except Exception as e:
+            print(f"Simplifier failure: {e}")
+    onnx.save(onnx_model, save_path)
+    print(f"ONNX export success, saved as {save_path}")
+
+
+if __name__ == "__main__":
+    main(parse_args())
--- rk_head.py
+++ rk_head.py
@@ -1,11 +1,17 @@
 import math
 import torch
+from ultralytics.utils.tal import make_anchors
 def detect_forward(self, x):
     shape = x[0].shape
+    if self.dynamic or self.shape != shape:
+            print(self.stride)
+            self.anchors, self.strides = (a.transpose(0, 1) for a in make_anchors(x, self.stride, 0.5))
+            self.shape = shape
     y = []
     for i in range(self.nl):
         y.append(self.cv2[i](x[i]))
         y.append(torch.sigmoid(self.cv3[i](x[i])))
+    print(f'self.anchors shape: {self.anchors.shape}')
     return y
 
 
@@ -21,6 +27,40 @@ def segment_forward(self, x):
         relocated.extend([mc[i]])
     relocated.extend([p])
     return relocated
+
+def pose_forward(self, x):
+    bs = x[0].shape[0]
+    kpt = [self.cv4[i](x[i]).view(bs, self.nk, -1) for i in range(self.nl)]
+    x = self.detect(self, x)
+    pred_kpts = self.kpt_decode(bs, kpt);
+    bo = len(x) // 3
+    relocated = []
+    for i in range(len(pred_kpts)):
+         relocated.extend(x[i*bo : (i + 1) * bo])
+         relocated.extend([pred_kpts[i]])
+    return relocated
+
+def pose_kpt_decode(self, bs, kpts):
+    ndim = self.kpt_shape[1]
+    # print(self.kpt_shape)
+    anch_len = []
+    anch_len.append(0)
+    pred_kpts = []
+    for i,kpt in enumerate(kpts):
+         anch_len.append(kpt.shape[2])
+         print(f'kpt shape: {kpt.shape}')
+         y = kpt.view(bs, *self.kpt_shape, -1)
+         print(f'y shape: {y.shape}, self.anchors shape: {self.anchors.shape}, self.strides shape: {self.strides.shape}')
+        #  print(f'--- shape : {y[:, :, :2].shape}')
+         start_idx = anch_len[i];
+         end_idx = anch_len[i + 1];
+    
+         a = (y[:, :, :2] * 2.0 + (self.anchors[:, start_idx:end_idx] - 0.5)) * self.strides[:, start_idx:end_idx]
+         if ndim == 3:
+            a = torch.cat((a, y[:, :, 2:3].sigmoid()), 2)
+         pred_kpts.extend([a.view(bs, self.nk, -1)])
+
+
 
 def obb_forward(self, x):
         """Concatenates and returns predicted bounding boxes and class probabilities."""
